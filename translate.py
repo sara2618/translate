e4779b1939eb599ab032a8f5f4e23e4eb8c90aff
sara2618 <r05922102@ntu.edu.tw>
2017-11-24 01:07:48 +0800

    add translate

---
+++ translate.py
@@ -0,0 +1,30 @@
+from googletrans import Translator
+import sys
+
+#read files
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+data = open(input_file, 'r', encoding = 'utf8') 
+data_lines = data.readlines()
+
+list = []
+
+#preprocess data
+for line in data_lines:
+	line = line.replace('\n', '')
+	line = line.replace('- ', '')
+	tmp_list = [x.strip() for x in line.split('.')]
+	list += tmp_list
+
+translator = Translator()
+
+#translate and write files
+with open(output_file, 'w') as text_file:
+	for item in list:
+		tmp = translator.translate(item, dest='zh-TW')
+		text_file.write(item + '.' + '\n')
+		text_file.write(tmp.text + '\n')
+		text_file.write('\n')
+
+text_file.close()
